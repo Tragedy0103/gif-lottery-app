14a7c08e6474bc5f9178893a0daedf15819caa8f
wei_wu <tragedy2271814@gmail.com>
2025-05-21 19:29:56 +0800

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/gif-lottery-app/', // 例如如果儲存庫名稱是 vue-project，則填入 /vue-project/
-})
