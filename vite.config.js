dc7c1070c1664e19d0d0e8654eaf060ce36cbbeb
wei_wu <tragedy2271814@gmail.com>
2025-05-21 18:51:12 +0800

    Add built files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/gif-lottery-app/', // 例如如果儲存庫名稱是 vue-project，則填入 /vue-project/
 })
